377b30ea207ad237ccb79c1d53d3af7ea5de0e86
yaburi <Nathan@192-168-1-7.tpgi.com.au>
2015-04-28 22:29:01 +1000

    fixed the last bit
    still a lot to go, just made the main function

--- testGame.c
+++ testGame.c
@@ -2,4 +2,13 @@
 // 
 
 #include <stdio.h>
-#include <std
+#include <stdlib.h>
+#include <assert.h>
+#include "Game-1.07.h"
+
+int main (int argc, char *argv[]) {
+
+    
+    printf ("All tests passed, you are Awesome!\n");
+
+}
