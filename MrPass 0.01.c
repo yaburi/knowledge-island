4aa8247d64c59bba02e0893a611fab00bdd87ebc
HollyAhel <holly.ahel@gmail.com>
2015-05-19 18:06:54 +1000

    started Arcs

--- MrPass 0.01.c
+++ MrPass 0.01.c
@@ -18,9 +18,19 @@ action decideAction (Game g) {
  
    action nextAction;
     
-    if (getStudents(Game g, getWhoseTurn(Game g), STUDENT_MJ) >= 1 && getStudents(game g, getWhoseTurn(Game g), STUDENT_MTV) >= 1 && getStudents(Game g, getWhoseTurn(Game g), STUDENT_MMONEY) >= 1) {
-        nextAction = 4; //start spin off
-    } else {
+    
+    if (getStudents(g, getWhoseTurn(g), STUDENT_MMONEY) >= 1 &&
+        getStudents(g, getWhoseTurn(g), STUDENT_BQN) >= 1) {
+        
+        nextAction.actionCode = OBTAIN_ARC;
+        // look at current campuses and where training centres are, decide on direction
+        // to build arc in.
+        
+    } else if (getStudents(g, getWhoseTurn(Game g), STUDENT_MJ) >= 1 &&
+        getStudents(g, getWhoseTurn(Game g), STUDENT_MTV) >= 1 &&
+        getStudents(g, getWhoseTurn(Game g), STUDENT_MMONEY) >= 1) {
+        nextAction.actionCode = START_SPINOFF; //start spin off
+    } else if {
         nextAction.actionCode = PASS;
     }
     
